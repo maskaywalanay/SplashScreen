c9f0df31b92f9b6af0195fb2b282b907c28429fa
maskaywalanay <mwalanay@gmail.com>
2019-12-06 10:25:23 +0700

    Tiga Komit

--- ASimpleSplashScreen/app/src/main/java/ngopi/pagi/com/SplashScreenSimple.java
+++ ASimpleSplashScreen/app/src/main/java/ngopi/pagi/com/SplashScreenSimple.java
@@ -1,6 +1,8 @@
 package ngopi.pagi.com;
 import android.app.*;
 import android.os.*;
+import java.io.*;
+import android.content.*;
 
 public class SplashScreenSimple extends Activity
 {
@@ -12,6 +14,23 @@ public class SplashScreenSimple extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
 		
+		Thread thread = new Thread(){
+			
+			public void run(){
+				try{
+					
+					sleep(3000);
+				}catch(InterruptedException e){
+					
+					e.printStackTrace();
+				} finally{
+					startActivity(new Intent(SplashScreenSimple.this,MainActivity.class));
+					finish();
+				}
+			}
+		};
+		
+		thread.start();
 	}
 	
 
